9b15b38e894321bb21ac5299039dc32bc9417bde
wingitman <dlblackburn111@gmail.com>
2024-04-24 23:41:29 +0100

    lazy + plug + working
    AppData > Local > nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,42 @@
-local o = vim.opt
-o.compatible = false
-o.number = true
-o.cmdheight = 2
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "hrsh7th/nvim-cmp", name = "nvim-cmp", priority = 1000 },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" } , -- not strictly required, but recommended
+  { "MunifTanjim/nui.nvim" },
+  { "nvim-neo-tree/neo-tree.nvim", name = "neo-tree" },
+
+  { "neovim/node-client" }
+
+}
+local opts = {}
+
+
+require("lazy").setup(plugins,opts)
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+print("hello world!")
+
+
+
+
