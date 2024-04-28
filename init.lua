eab54b0d4bcc9c9e0ccef664b94a7cc7c3287af6
wingitman <dlblackburn111@gmail.com>
2024-04-28 18:28:27 +0100

    lsp setup

--- init.lua
+++ init.lua
@@ -13,5 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
 require("vim-options")
-require("lazy").setup("plugins") 
+require("lazy").setup("plugins")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return  {
-  'nvim-telescope/telescope.nvim',
+  {  'nvim-telescope/telescope.nvim',
   tag = "0.1.5",
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
@@ -7,5 +7,20 @@ return  {
     vim.keymap.set("n","<leader>fg",builtin.live_grep, {})
     vim.keymap.set("n","<C-p>", builtin.find_files, {})
   end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
  
