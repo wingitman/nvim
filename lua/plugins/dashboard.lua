a18ab407c515dd4a9670fd5bcce20471c5c5aa37
wingitman <dlblackburn111@gmail.com>
2024-08-10 11:24:50 +0100

    theming
    Removed theming in favour of terminal themes
    updated some configs
    updated tmux conf

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,7 +1,118 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
-	end,
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  enabled = true,
+  init = false,
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    -- Define and set highlight groups for each logo line
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#311B92" })  -- Indigo
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#512DA8" })  -- Deep Purple
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#673AB7" })  -- Deep Purple
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#9575CD" })  -- Medium Purple
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#B39DDB" })  -- Light Purple
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#D1C4E9" })  -- Very Light Purple
+    vim.api.nvim_set_hl(0, "NeovimDashboardUsername", { fg = "#D1C4E9" }) -- light purple
+    dashboard.section.header.type = "group"
+    dashboard.section.header.val = {
+      {
+        type = "text",
+        val = "   ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+        opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "text",
+        val = "   ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+        opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "text",
+        val = "   ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+        opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "text",
+        val = "   ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+        opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "text",
+        val = "   ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+        opts = { hl = "NeovimDashboardLogo5", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "text",
+        val = "   ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+        opts = { hl = "NeovimDashboardLogo6", shrink_margin = false, position = "center" },
+      },
+      {
+        type = "padding",
+        val = 1,
+      },
+      {
+        type = "text",
+        val = "𝙾𝚑 𝚝𝚑𝚎 𝚓𝚘𝚢 𝚘𝚏 𝚑𝚊𝚟𝚒𝚗𝚐 𝚢𝚘𝚞𝚛 𝚘𝚠𝚗 𝚌𝚞𝚜𝚝𝚘𝚖 𝚝𝚎𝚡𝚝 𝚎𝚍𝚒𝚝𝚘𝚛 :)",
+        opts = { hl = "NeovimDashboardUsername", shrink_margin = false, position = "center" },
+      },
+    }
+    -- stylua: ignore
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+      dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+      dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+      dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+      dashboard.button("Z", " " .. " Open Directories", "<cmd> lua require('telescope').extensions.zoxide.list() <cr>"),
+      dashboard.button("c", " " .. " Config", "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+      --dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+    }
+    vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#311B92" }) -- Dark Indigo
+    vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#D1C4E9" }) -- Light Purple
+    vim.api.nvim_set_hl(0, "AlphaShortcut", { fg = "#8BC34A" }) -- Greenish
+    vim.api.nvim_set_hl(0, "AlphaFooter", { fg = "#edd691" })
+
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 3
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    -- close Lazy and re-open when the dashboard is ready
+    if vim.o.filetype == "lazy" then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "AlphaReady",
+        callback = function()
+          require("lazy").show()
+        end,
+      })
+    end
+
+    require("alpha").setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd("User", {
+      once = true,
+      pattern = "LazyVimStarted",
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
+  end,
 }
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,22 +1,59 @@
 return {
-  { "catppuccin/nvim" },
-  { "eldritch-theme/eldritch.nvim" },
-  { "scottmckendry/cyberdream.nvim" },
-  { "sainnhe/everforest" },
-  { "shaunsingh/nord.nvim" },
-  { "Yazeed1s/minimal.nvim" },
-  { "rose-pine/neovim" },
   {
-    "folke/tokyonight.nvim",
-    config = function()
-      --vim.cmd.colorscheme "rose-pine"
-      --vim.cmd.colorscheme "minimal"
-      --vim.cmd.colorscheme "nord"
-      --vim.cmd.colorscheme "everforest"
-      --vim.cmd.colorscheme "eldritch"
-      --vim.cmd.colorscheme "cyberdream"
-      vim.cmd.colorscheme "tokyonight"
-      --vim.cmd.colorscheme "catppuccin"
-    end
-  },
+		dependencies = {
+			{
+				"catppuccin/nvim",
+				name = "catppuccin",
+				priority = 1000,
+				config = function()
+					vim.cmd.colorscheme("catppuccin")
+				end,
+			},
+			{ "eldritch-theme/eldritch.nvim" },
+			{ "scottmckendry/cyberdream.nvim" },
+			{ "sainnhe/everforest" },
+			{ "shaunsingh/nord.nvim" },
+			{ "Yazeed1s/minimal.nvim" },
+			{ "rose-pine/neovim" },
+      
+			{ "rebelot/kanagawa.nvim" },
+		},
+		config = function()
+			--vim.cmd.colorscheme "rose-pine"
+			--vim.cmd.colorscheme "minimal"
+			--vim.cmd.colorscheme "nord"
+			--vim.cmd.colorscheme "everforest"
+			--vim.cmd.colorscheme "eldritch"
+			--vim.cmd.colorscheme "cyberdream"
+			--vim.cmd.colorscheme("tokyonight")
+			--vim.cmd.colorscheme "catppuccin"
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = true, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				}
+        --[[
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "dragon", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "dragon", -- try "dragon" !
+					light = "lotus",
+				},
+        ]]
+			})
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
 }
